8453ac2b3034f8aa360a0baa472feb4b118aef55
Matt110110 <matruprasadchand@gmail.com>
2018-01-24 20:39:02 +0530

    Quizzler App by The London Brewery

---
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.londonappbrewery.quizzler;
+
+/**
+ * Created by Matt on 24-01-2018.
+ */
+
+public class TrueFalse {
+    private int mQuestionID;
+    private boolean mAnswer;
+    public TrueFalse (int questionID, boolean answer) {
+        mQuestionID = questionID;
+        mAnswer = answer;
+    }
+
+    public int getQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        mQuestionID = questionID;
+    }
+
+    public boolean isAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(boolean answer) {
+        mAnswer = answer;
+    }
+}
